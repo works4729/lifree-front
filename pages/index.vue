99d363ef873b054f38d042f388a62229575259a1
works4729 <r.works4729@gmail.com>
2022-06-29 03:26:20 +0000

    Add Vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -50,60 +50,59 @@
 
 
 
-<template>
-  <v-container fluid>
-    <v-card
-      flat
-      tile
-      color="transparent"
-    >
-      ...
+<!--<template>-->
+<!--  <v-container fluid>-->
+<!--    <v-card-->
+<!--      flat-->
+<!--      tile-->
+<!--      color="transparent"-->
+<!--    >-->
+<!--      ...-->
       <!-- 追加 -->
-      <v-card-title>
-        VuetifyカスタムCSSの検証
-      </v-card-title>
-      <v-card-text>
-        ipad（768px）とmobile（426px）で表示・非表示
-      </v-card-text>
-      <v-card-text>
-        <v-card
-          v-for="(cls, i) in customClass"
-          :key="`cls-${i}`"
-          :color="cls.color"
-          :class="cls.name"
-        >
-          <v-card-text>
-            {{ cls.des }}
-          </v-card-text>
-        </v-card>
-      </v-card-text>
+<!--      <v-card-title>-->
+<!--        VuetifyカスタムCSSの検証-->
+<!--      </v-card-title>-->
+<!--      <v-card-text>-->
+<!--        ipad（768px）とmobile（426px）で表示・非表示-->
+<!--      </v-card-text>-->
+<!--      <v-card-text>-->
+<!--        <v-card-->
+<!--          v-for="(cls, i) in customClass"-->
+<!--          :key="`cls-${i}`"-->
+<!--          :color="cls.color"-->
+<!--          :class="cls.name"-->
+<!--        >-->
+<!--          <v-card-text>-->
+<!--            {{ cls.des }}-->
+<!--          </v-card-text>-->
+<!--        </v-card>-->
+<!--      </v-card-text>-->
       <!-- ここまで -->
-    </v-card>
-  </v-container>
-</template>
+<!--    </v-card>-->
+<!--  </v-container>-->
+<!--</template>-->
 
-<script>
-export default {
-  data () {
-    return {
-      colors: ['primary', 'info', 'success', 'warning', 'error', 'background'],
-      // 追加
-      customClass: [
-        { name: 'hidden-ipad-and-down', color: 'error', des: 'ipad未満で隠す' },
-        { name: 'hidden-ipad-and-up', color: 'info', des: 'ipad以上で隠す' },
-        { name: 'hidden-mobile-and-down', color: 'success', des: 'mobile未満で隠す' },
-        { name: 'hidden-mobile-and-up', color: 'warning', des: 'mobile以上で隠す' }
-      ]
-    }
-  }
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--  data () {-->
+<!--    return {-->
+<!--      colors: ['primary', 'info', 'success', 'warning', 'error', 'background'],-->
+<!--      customClass: [-->
+<!--        { name: 'hidden-ipad-and-down', color: 'error', des: 'ipad未満で隠す' },-->
+<!--        { name: 'hidden-ipad-and-up', color: 'info', des: 'ipad以上で隠す' },-->
+<!--        { name: 'hidden-mobile-and-down', color: 'success', des: 'mobile未満で隠す' },-->
+<!--        { name: 'hidden-mobile-and-up', color: 'warning', des: 'mobile以上で隠す' }-->
+<!--      ]-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 
 
 
 <template>
   <v-app>
-    <home-app-bar />
+    <wel-app-bar />
     <v-sheet>
       <v-container
         fluid
